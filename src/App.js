494d16fe863d7db60990647e5e134a98f59bf335
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-19 16:12:58 -0500

    [+] Component > Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 
 /** Component */
+import Header from './components/Header';
 import FormSearch from './components/FormSearch';
 
 /** Context */
@@ -9,6 +10,7 @@ import CategoriesProvider from './context/categories-context';
 function App() {
   return (
     <CategoriesProvider>
+      <Header />
       <FormSearch />
     </CategoriesProvider>
   );
