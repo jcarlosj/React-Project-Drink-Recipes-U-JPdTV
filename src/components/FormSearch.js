b9f83d349cd829ba6a3ae3a6dba4f0431fe92ee7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-20 09:42:11 -0500

    [+] FormSearch > setState: dataForm & onChange (get form data)

--- src/components/FormSearch.js
+++ src/components/FormSearch.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { CategoriesContext } from '../context/categories-context';
 
 const FormSearch = () => {
@@ -6,7 +6,21 @@ const FormSearch = () => {
     /** Hook: Context */
     const { categories } = useContext( CategoriesContext );     // Makes context data available 
 
+    /** Hook: Define State */
+    const [ dataForm , setDataForm ] = useState({
+        ingredient: '',
+        category: ''
+    });
+
     console .log( 'Categorias', categories );
+
+    /** Update form field values */
+    const onChangeDataForm = event => {
+        setDataForm({
+            ...dataForm,
+            [ event .target .name ]: event .target .value
+        });
+    }
 
     return (
         <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarTogglerDemo02">
@@ -16,10 +30,12 @@ const FormSearch = () => {
                     name="ingredient"
                     placeholder="Ingrediente" 
                     className="form-control mr-sm-2" 
+                    onChange={ onChangeDataForm }
                 />
                 <select 
                     name="category"
                     className="form-control mr-sm-2"
+                    onChange={ onChangeDataForm }
                 >
                     <option value="">Selecciona categoría...</option>
                     { categories .map( ( category, index ) => (
