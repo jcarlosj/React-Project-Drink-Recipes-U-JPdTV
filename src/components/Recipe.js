f09a63bcb6f740fe420ad7cf4f34b527504aafd0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-20 17:13:59 -0500

    [+] Recipe > @material-ui: Display Modal Window

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,9 +1,53 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 /** Context */
 import { ModalContext } from '../context/modal-context';
 
+/** Material UI */
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+/** Modal: Material UI */
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+  
+const useStyles = makeStyles( theme => ({
+    paper: {
+        position: 'absolute',
+        width: 600,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
+
+
+/** Component */
 const Recipe = ({ recipe }) => {        // Destructuring Props
+
+    /** Hook: Define State (Configuración para Material UI)
+     * getModalStyle no es una función pura, rodamos el estilo solo en el primer render */
+    const 
+        [ modalStyle ] = useState( getModalStyle ),
+        [ open, setOpen ] = useState( false );
+
+    const classes = useStyles();
+
+    const handleOpen = () => {
+        setOpen( true );
+    };
+
+    const handleClose = () => {
+        setOpen( false );
+    };
 
     /** Hook: Context */
     const { setIdRecipe } = useContext( ModalContext );        // Makes context data available 
@@ -24,8 +68,26 @@ const Recipe = ({ recipe }) => {        // Destructuring Props
                     <button 
                         type="button" 
                         className="btn btn-secondary btn-lg btn-block"
-                        onClick={ () => setIdRecipe( idDrink ) }
+                        onClick={ () => {
+                            setIdRecipe( idDrink )
+                            handleOpen();
+                        }}
                     >Ver receta</button>
+
+                    <Modal
+                        open={ open }               // Open the modal window
+                        onClose={ () => {
+                            handleClose();          // Close the modal window
+                            setIdRecipe( null );    // Update State 'idRecipe' of 'ModalContext'
+                        }}
+                    >
+                        <div
+                            style={ modalStyle }          // Modal window position CSS style
+                            className={ classes .paper }  // General CSS styles of the modal window ('paper' css class name)
+                        >
+                            <h2>Dentro de la ventana modal</h2>
+                        </div>
+                    </Modal>
                 </div>
             </div>
 
